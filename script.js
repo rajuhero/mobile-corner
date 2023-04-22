691e893b61d1e583546fabb70c192a787b27c22a
raju ahmed <fmraju32q@gmail.com>
2023-04-22 19:47:03 +0300

    add search inpot

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+
+
+
+
+  
+
+  
+
+
+
+
+
 const MobileApi = ()=>{
-const   URL = ' https://openapi.programming-hero.com/api/phones?search=iphone'
+
+const   URL = 'https://openapi.programming-hero.com/api/phones?search=samsung'
 fetch(URL)
 .then(res => res.json())
 .then(data => showData(data.data))
@@ -8,12 +21,35 @@ fetch(URL)
 
 
 const showData = mobileDta =>{
+
+    const parentDiv = document.getElementById("parent-dive");
+    
     for(const mobile of mobileDta){
-        console.log(mobile)
+     
+       
 
+        const  newDom = document.createElement("div");
+       newDom.classList.add('card');
 
+        newDom.innerHTML =`
+        <div class=" card card-compact w-70 ml-10 bg-base-100 shadow-xl flex">
+            <figure><img src="${mobile.image}" alt="Shoes" /></figure>
+            <div class="card-body">
+              <h2 class="${mobile.data}">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        
+        
+        `
+        parentDiv.appendChild(newDom);
+        console.log(mobile.image)
 
     }
+       
 
 
     
@@ -24,4 +60,29 @@ const showData = mobileDta =>{
 
 
 MobileApi();
+
+
+//  start login page
+
+ const loginBtn= document.getElementById('login').addEventListener('click',function(){
+  console.log('hi');
+ const loginEmwil = document.getElementById('email');
+const newEmeil = loginEmwil.value;
+
+ const loginpassword= document.getElementById('Password');
+ const newLoginPass = loginpassword.value;
+ if(newEmeil === "rajuahmed@gamil.com" & newLoginPass ==="321"){
+  window.location.href="api.html";
+
+ }
+ else{
+
+  alert('salami nita hole email password thik vabe lekhte hobe')
+ }
+
+
+
+
+ });
+
 
